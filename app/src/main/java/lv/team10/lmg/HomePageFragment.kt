0a0f5a795b7ddaff9956e0be89d70e0534c06411
gisis26.01@inbox.lv <gisis26.01@inbox.lv>
2023-05-17 21:42:46 +0300

    removed toolbar

--- app/src/main/java/lv/team10/lmg/HomePageFragment.kt
+++ app/src/main/java/lv/team10/lmg/HomePageFragment.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import lv.team10.lmg.databinding.FragmentFirstBinding
+import lv.team10.lmg.databinding.FragmentHomepageBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class HomePageFragment : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: FragmentHomepageBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class HomePageFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = FragmentHomepageBinding.inflate(inflater, container, false)
         return binding.root
 
     }

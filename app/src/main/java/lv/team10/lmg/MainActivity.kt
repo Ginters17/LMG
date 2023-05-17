4a0e58533ca5053333ac7e72da2e0fbcc8c11b4a
Artis Cveks <artis.cveks@example.com>
2023-05-17 22:15:18 +0300

    Changed App bar

--- app/src/main/java/lv/team10/lmg/MainActivity.kt
+++ app/src/main/java/lv/team10/lmg/MainActivity.kt
@@ -1,13 +1,11 @@
 package lv.team10.lmg
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
 import lv.team10.lmg.databinding.ActivityMainBinding
@@ -24,11 +22,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAnchorView(R.id.fab)
-                    .setAction("Action", null).show()
+        binding.appbarButtonLogin.setOnClickListener {
+            val navController = findNavController(R.id.nav_host_fragment_content_main)
+            if (navController.currentDestination?.id != R.id.loginFragment) {
+                navController.navigate(R.id.action_FirstFragment_to_loginFragment)
+            }
         }
     }
 
